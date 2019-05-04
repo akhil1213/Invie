e3d7fdf3eedb15466a0f0c2086d1e87689e90024
Dweej Patel <dweejpatel97@gmail.com>
2019-05-04 15:39:02 -0400

    added appropriate routes

--- server/routes/investee.js
+++ server/routes/investee.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const InvesteeController = require("../controller/investee");
+const BusinessIdeaController = require("../controller/businessIdea");
 const midAuth = require("../tokenAuth/authInvestee");
 
 const router = express.Router();
@@ -9,6 +10,12 @@ router.post("/signup", InvesteeController.createInvestee);
 
 router.post("/login", InvesteeController.loginInvestee);
 
-router.patch("/updateDesc", midAuth,InvesteeController.updateInvestee);
+router.post("/createIdea", midAuth, BusinessIdeaController.createBusinessIdea);
+
+router.get("/getIdea", midAuth, BusinessIdeaController.getBusinessIdea);
+
+router.patch("/updateIdea", midAuth, BusinessIdeaController.updateBusinessIdea);
+
+router.patch("/updateDesc", midAuth, InvesteeController.updateInvestee);
 
 module.exports = router;
