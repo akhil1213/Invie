11c10b72a831e5610b61d758739227323431db25
Dweej Patel <dweejpatel97@gmail.com>
2019-05-04 13:01:49 -0400

    file addition for businessIdea backend

---
+++ server/controller/businessIdea.js
@@ -0,0 +1,14 @@
+const BusinessIdea = require("../models/BusinessIdea");
+
+
+exports.createBusinessIdea = (req, res, next) => {
+
+}
+
+exports.getBusinessIdea = (req, res, next) => {
+
+}
+
+exports.updateBusinessIdea = (req, res, next) => {
+
+}
