12fec222d2a7bee00974fe602cf47c5fcf347d46
GizmoAaron <Komal.Narine52@myhunter.cuny.edu>
2019-05-04 17:36:56 -0400

    GET Controler for BusinessIdea

--- server/controller/businessIdea.js
+++ server/controller/businessIdea.js
@@ -20,7 +20,24 @@ exports.createBusinessIdea = (req, res, next) => {
 }
 
 exports.getBusinessIdea = (req, res, next) => {
-
+  BusinessIdea.findOne({userId: req.data.investeeId})
+	.then(BusinessIdea =>{
+	   if(!BusinessIdea) {
+	     return res.status(404).json({
+	     	message: "Object cannot be found"
+	     });
+	   }
+	   return res.status(200).json({
+	     BusinessIdea: BusinessIdea,
+	       message: "Object found"
+	   });
+	 })
+	.catch(error => {
+	  console.log(error);
+	  return res.status(401).json({
+	  	message: error
+	  });
+	});
 }
 
 exports.updateBusinessIdea = (req, res, next) => {
